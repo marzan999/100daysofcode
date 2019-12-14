4ad9dc19256cfb9cb614e6f217a5bf703f5b4650
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2019-12-14 17:57:47 +0600

    Add files via upload

---
+++ Day_28.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  printf("Enter an integer\n");
+  scanf("%d",&n);
+
+  if(n%2==0)
+    printf("This is Even\n");
+  else
+    printf("This is Odd\n");
+
+  return 0;
+}
