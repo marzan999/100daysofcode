703e6b29e56fabde65850f1ca36f3d9fb7d04601
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2019-12-15 19:23:09 +0600

    Add files via upload

---
+++ Day_29.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char c;
+    int lowercase,uppercase;
+    printf("Enter an alphabet: ");
+    scanf("%c",&c);
+    lowercase=(c=='a'||c=='e'||c=='i'||c=='o'||c=='u');
+    uppercase=(c=='A'||c=='E'||c=='I'||c=='O'||c=='U');
+    if (lowercase||uppercase)
+        printf("%c is a vowel.",c);
+    else
+        printf("%c is a consonant.",c);
+    return 0;
+}
