4abcf7c9650e6f79c428a10656668f3f69c2727e
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2020-01-27 21:03:24 +0600

    Add files via upload

---
+++ Day_47.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i=0;
+    cin>>n;
+    while(n--)
+    {
+        cout<<"Ho";
+        i++;
+        if(n!=0)
+        cout<< " ";
+    }
+    cout<<"!"<<endl;
+    return 0;
+}
