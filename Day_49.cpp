215d27f0fe069af431e70fdb86c7858956c98880
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2020-02-03 18:32:27 +0600

    Add files via upload

---
+++ Day_49.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int T,a,b,i;
+    cin>>T;
+    for(i=0,a,b;i<T;i++)
+    {
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+    return 0;
+}
