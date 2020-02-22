3eb48d074e6e4c49eea5f7fd0bb8f04f9f8cc4c1
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2020-02-22 21:27:05 +0600

    Add files via upload

---
+++ Day_56.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout.precision(10);
+    cout<<fixed;
+    double r;
+    float a;
+    cin>>a;
+    r=3.141592653589793*a*a;
+    cout<<r;
+    return 0;
+}
+
