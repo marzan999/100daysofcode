1c3424efba4beed76f761f02bc3d377975ae6788
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2020-03-08 20:32:57 +0600

    Add files via upload

---
+++ Day_68.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    cin>>n;
+    for(i=0; i<n; i++)
+    {
+        for(j=i; j<n-1; j++)
+        {
+            cout<<" ";
+        }
+        for(j=0; j<=i; j++)
+        {
+            if(j==i)
+                cout<<"*";
+            else
+                cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
